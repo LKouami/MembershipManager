9e218a9e333302a48ffca198701e4e4de466a6d3
Lucas Kouami <rkouami@outlook.com>
2022-08-22 23:20:17 +0000

    disable authorize

--- MembershipManager/Controllers/CommunesController.cs
+++ MembershipManager/Controllers/CommunesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MembershipManager.Controllers
 {
-        [Authorize]
+        // [Authorize]
         [ApiController]
         [Route("api/v1/[controller]")]
         public class CommunesController : ControllerBase
